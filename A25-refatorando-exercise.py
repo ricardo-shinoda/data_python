a977cf3535f94bcc7269b172cbb8a313888a26d7
ricardo-shinoda <ricardoshinoda@gmail.com>
2022-12-04 12:23:12 -0300

    refactoring: counting and printing

--- A25-refatorando-exercise.py
+++ A25-refatorando-exercise.py
@@ -1,19 +1,24 @@
 import time as t
 import matplotlib.pyplot as plt
 
-word_list = []
-start = []
-end = []
-counted = []
+duration_list = [] # here will be Y axle
+x = [] # here will be X axle
+vez = 0
 
-def count():
-    while len(list) < 5:
-        starting = t.perf_counter()
-        msg = input('Type a word: ')
-        ending = t.perf_counter()
-        word_list.append(msg)
-        start.append(starting)
-        end.append(ending)
-        time_list = (ending - starting)
-        count_time.append(time_list)
+while len(duration_list) < 5:
+    start = t.perf_counter()
+    input('Type something: ')
+    end = t.perf_counter()
+    duration = round(end - start, 2)
+    duration_list.append(duration)
+    x.append(vez + 1)
+    vez += 1
 
+print(duration_list)
+print(x)
+
+legend = ['first','second', 'third', 'fourth', 'fifth']
+plt.xticks(x, legend) # To change x value to legend
+
+plt.plot(x, duration_list)
+plt.show()
