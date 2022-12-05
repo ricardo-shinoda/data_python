5b90ab0d45435978186a0b2da82935e83d5e2d38
ricardo-shinoda <ricardoshinoda@gmail.com>
2022-12-04 21:50:19 -0300

    Finishing exercise classe 25 - refectoring with dinamic inputs

--- A25-refatorando-exercise.py
+++ A25-refatorando-exercise.py
@@ -2,23 +2,23 @@ import time as t
 import matplotlib.pyplot as plt
 
 duration_list = [] # here will be Y axle
-x = [] # here will be X axle
-vez = 0
+vez = 1
+repeat = 9
+legend = [] # this will be the X axle
 
-while len(duration_list) < 5:
+print('Esse programa contará o intervalo entre as digitações das palavras por', repeat, 'vezes')
+while len(duration_list) < repeat:
     start = t.perf_counter()
     input('Type something: ')
     end = t.perf_counter()
     duration = round(end - start, 2)
     duration_list.append(duration)
-    x.append(vez + 1)
+    legend_str = str(vez) + ' tentativa'
+    legend.append(legend_str)
     vez += 1
 
 print(duration_list)
-print(x)
+print(legend)
 
-legend = ['first','second', 'third', 'fourth', 'fifth']
-plt.xticks(x, legend) # To change x value to legend
-
-plt.plot(x, duration_list)
+plt.plot(legend, duration_list)
 plt.show()
