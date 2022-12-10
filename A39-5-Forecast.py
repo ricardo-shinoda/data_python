d417ac014eea6247ed0082bbd835484cacd36bae
ricardo-shinoda <ricardoshinoda@gmail.com>
2022-12-10 17:36:43 -0300

    feat: weather forecast, need do save in a loop

--- A39-5-Forecast.py
+++ A39-5-Forecast.py
@@ -22,19 +22,34 @@ def getCoordinates():
         except:
             print('Error on get coordinates')
 
-def localCode(lat, long):
+def getLocalCode(lat, long):
     locationAPIUrl = "http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=" + apiKey + "&q=" + lat + "%2C" + long + "&language=pt-br"
     r = requests.get(locationAPIUrl)
     if (r.status_code != 200):
         print('Não foi possível obter a localização (localCode)')
         return None
     else:
-        locationResponse = (json.loads(r.text))
-        print(pprint.pprint(locationResponse))
-        localInfo = {}
-        localInfo['localName'] = locationResponse['LocalizedName'] + " , " + locationResponse['AdministrativeArea']['LocalizedName'] + ", " + locationResponse['Country']['LocalizedName']
-        localInfo['localCode'] = locationResponse['Key']
-        return localInfo
+        try:
+            localCodeResponse = (json.loads(r.text))
+            # print(pprint.pprint(localCodeResponse))
+            localInfo = {}
+            localInfo['localName'] = localCodeResponse['LocalizedName'] + " , " + localCodeResponse['AdministrativeArea']['LocalizedName'] + " - " + localCodeResponse['Country']['LocalizedName']
+            localInfo['localCode'] = localCodeResponse['Key']
+            print(localInfo)
+            return localInfo
+        except:
+            return None
+
+def getWeatherForecast(localCode, localName):
+    weatherForecast = "http://dataservice.accuweather.com/forecasts/v1/daily/5day/" + localCode + "?apikey=" + apiKey + "&language=pt-br&metric=true"
+    r = requests.get(weatherForecast)
+    if (r.status_code != 200):
+        print('Não foi possível obter a previsão do tempo, tente novamente.')
+        return None
+    else:
+        forecastResponse = (json.loads(r.text))
+        print(pprint.pprint(forecastResponse))
 
 coordinates = getCoordinates()
-local = localCode(coordinates['lat'], coordinates['long'])
+local = getLocalCode(coordinates['lat'], coordinates['long'])
+forecast = getWeatherForecast(local['localCode'], local['localName'])
