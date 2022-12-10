49c5c3c76bb5bae34ee5bc68784d1f5946d2c8e6
ricardo-shinoda <ricardoshinoda@gmail.com>
2022-12-10 17:04:29 -0300

    feat: Location Code

--- A39-5-Forecast.py
+++ A39-5-Forecast.py
@@ -12,8 +12,29 @@ def getCoordinates():
     else:
         try:
             location = json.loads(r.text)
-            print(pprint.pprint(location))
+            # print(pprint.pprint(location)) # to look at the api output
+            coordinate = {}
+            coordinate['long'] = location['geoplugin_longitude']
+            coordinate['lat'] = location['geoplugin_latitude']
+            print(coordinate['long'])
+            print(coordinate['lat'])
+            return coordinate
         except:
             print('Error on get coordinates')
 
-getCoordinates()
+def localCode(lat, long):
+    locationAPIUrl = "http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=" + apiKey + "&q=" + lat + "%2C" + long + "&language=pt-br"
+    r = requests.get(locationAPIUrl)
+    if (r.status_code != 200):
+        print('Não foi possível obter a localização (localCode)')
+        return None
+    else:
+        locationResponse = (json.loads(r.text))
+        print(pprint.pprint(locationResponse))
+        localInfo = {}
+        localInfo['localName'] = locationResponse['LocalizedName'] + " , " + locationResponse['AdministrativeArea']['LocalizedName'] + ", " + locationResponse['Country']['LocalizedName']
+        localInfo['localCode'] = locationResponse['Key']
+        return localInfo
+
+coordinates = getCoordinates()
+local = localCode(coordinates['lat'], coordinates['long'])
