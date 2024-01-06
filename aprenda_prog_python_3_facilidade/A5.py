74287332b9bc3b476218f8d86936f5d709ce976d
ricardo-shinoda <ricardoshinoda@gmail.com>
2024-01-06 16:38:38 -0300

    class: 5

---
+++ aprenda_prog_python_3_facilidade/A5.py
@@ -0,0 +1,37 @@
+# num1 = 2
+# num2 = 2.5
+
+# print(type(num1))
+# print(type(num2))
+
+# # potência
+
+# print(num1**5)
+
+# # boolean
+
+# print(num1 > num2)
+
+
+# Exercise I
+# age = 18
+
+# if age >= 18:
+#     print('Access permited')
+# else:
+#     print('Access not permited')
+
+# Exercise 2
+
+# print('My first Python program')
+# num1 = int(input('Type a number:'))
+# num2 = int(input('Type a number:'))
+# print('O resultado de', num1, "+", num2, "é:", num1+num2)
+
+# Exercise 3
+
+peso1 = float(input('Peso 1: '))
+peso2 = float(input('Peso 2: '))
+nota1 = float(input('nota 1: '))
+nota2 = float(input('nota 2: '))
+print('média ponderada é:', ((peso1*nota1)+(peso2*nota2)) / (nota1+nota2))
