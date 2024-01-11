1e19cb122fda3ed5015ba015832f714ad6f45a49
ricardo-shinoda <ricardoshinoda@gmail.com>
2024-01-10 22:01:47 -0300

    class A9

--- aprenda_prog_python_3_facilidade/A9_dictionary.py
+++ aprenda_prog_python_3_facilidade/A9_dictionary.py
@@ -1,8 +1,8 @@
 color = {'vermelho': 'red', 'verde': 'green',
          'azul': 'blue', 'amarelo': 'yellow'}
-# cor = input('Digite uma cor: ').lower()  # transformar tudo em letra minúscula
-# traducao = color.get(cor, 'Esta cor não consta no meu dicionário')
-# print(traducao)
+cor = input('Digite uma cor: ').lower()  # transformar tudo em letra minúscula
+traducao = color.get(cor, 'Esta cor não consta no meu dicionário')
+print(traducao)
 
 # The type
 print(type(color))
@@ -15,3 +15,19 @@ del color['vermelho']
 
 # To know if there is a key in the dictionary
 print('verde' in color)
+
+# Loop for
+
+for x in color:
+    print(x)
+
+for i in color:
+    print(i+': '+color[i])
+
+# Too look something inside a dictionary and return a message
+
+print(color.get('verde', 'This information is not registered'))
+
+# How to clear a dictionary and delete everything inside it?
+
+# color.clear()
