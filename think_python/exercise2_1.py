601be898568b8c6defcf96068df0a0ae26cff57e
ricardo-shinoda <ricardoshinoda@gmail.com>
2024-01-15 21:38:46 -0300

    capitulo 2

---
+++ think_python/exercise2_1.py
@@ -0,0 +1,10 @@
+
+# Como calcular o percentual de hora de determinado minuto
+
+minute = 6
+percentage = (minute * 100) / 60
+print(percentage)
+
+x = y = 1
+
+print(x).
---
+++ think_python/exercise2_2.py
@@ -0,0 +1,34 @@
+r = 5
+pi = 3.141592653589793
+
+
+volume = 4 / 3 * pi * r**3
+print(volume)
+
+# segundo
+
+livro = 24.95
+desconto = 0.4
+transporte_um = 3
+transporte_demais = 0.75
+total = 60
+
+livro_descontado = livro - (livro * desconto)
+custo = (livro_descontado * total) + transporte_um + (transporte_demais * 59)
+print('Custo total para 60 livros é: ', custo)
+
+# Terceiro
+
+hora = 3600
+minuto = 60
+
+primeiro_pace = 8 * minuto + 15
+segundo_pace = 7 * minuto + 12
+terceiro_pace = primeiro_pace
+
+tempo_total = primeiro_pace + 3 * segundo_pace + terceiro_pace
+print(tempo_total / 60)
+
+# resultado 38.1 = 38 minutos e 6 segundos
+# Saiu  6:52
+# Chegou 7:40:06
