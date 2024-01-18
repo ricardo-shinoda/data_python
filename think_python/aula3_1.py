31bf07898abf42a7bf021b2975893dd8513d01db
ricardo-shinoda <ricardoshinoda@gmail.com>
2024-01-17 22:12:59 -0300

    functions def

--- think_python/aula3_1.py
+++ think_python/aula3_1.py
@@ -11,3 +11,10 @@ print(float(outro))
 # Converter o argumento em string
 
 print(str(outro))
+
+# Como acrescentar funções dentro de funções
+
+
+def print_lyrics():
+    print("I'm a lumberjack, and I'm okay.")
+    print("I sleep all night and I work all day.")
