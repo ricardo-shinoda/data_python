83705112c4ac2aec2571d1a7e959d08b18f52265
ricardo-shinoda <ricardoshinoda@gmail.com>
2024-01-23 20:07:18 -0300

    exercise 3

--- think_python/exercise3_2.py
+++ think_python/exercise3_2.py
@@ -1,10 +1,31 @@
-def do_twice(f):
-    f()
-    f()
+def do_twice(f, arg):
+    f(arg)
+    f(arg)
+
+
+def print_twice(arg):
+    print(arg)
+    print(arg)
+
+
+def do_four(f, arg):
+    do_twice(f, arg)
+    do_twice(f, arg)
+
+# Testando do_twice com print_spam
 
 
 def print_spam():
     print('spam')
 
 
-do_twice(print_spam)
+print("Chamando do_twice com print_spam:")
+do_twice(print_spam, "spam")
+
+# Testando do_twice com print_twice e 'spam' como argumento
+print("\nChamando do_twice com print_twice:")
+do_twice(print_twice, "spam")
+
+# Chamando do_four com print_twice e 'spam' como argumento
+print("\nChamando do_four com print_twice:")
+do_four(print_twice, "spam")
