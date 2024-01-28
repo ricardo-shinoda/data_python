7ac51de7b1cb4e89420012163f3a4f8b84d3edb1
ricardo-shinoda <ricardoshinoda@gmail.com>
2024-01-27 21:58:02 -0300

    exercise4.3 done

--- think_python/exercise4_3.py
+++ think_python/exercise4_3.py
@@ -24,19 +24,37 @@ def polygon(t, n, length):
 # polygon(bob, n=7, length=70)
 
 
-def circle(t, r):
-    circumference = 2 * math.pi * r
-    n = 50
-    length = circumference / n
-    polygon(t, n, length)
+# def circle(t, r):
+#     circumference = 2 * math.pi * r
+#     n = 50
+#     length = circumference / n
+#     polygon(t, n, length)
 
 
 # circle(bob, 200)
 
 
+def polygon(t, n, length):
+    angle = 360.0 / n
+    polyline(t, n, length, angle)
+
+
+def polyline(t, n, length, angle):
+    for i in range(n):
+        t.fd(length)
+        t.lt(angle)
+
+
 def arc(t, r, angle):
-    polygon(t, 70, angle)
+    arc_length = 2 * math.pi * r * angle / 360
+    n = int(arc_length / 3) + 1
+    step_length = arc_length / n
+    step_angle = float(angle) / n
+    polyline(t, n, step_length, step_angle)
 
 
-# circle(bob, 1000)
-# arc(bob, 2, 360)
+def circle(t, r):
+    arc(t, r, 360)
+
+
+circle(bob, 100)
