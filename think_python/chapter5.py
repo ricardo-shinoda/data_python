45f329cdc5fdec417b8b95e3505a8915b5f8025a
Ricardo Shinoda <ricardoshinoda@gmail.com>
2024-01-31 20:50:35 -0300

    rest

--- think_python/chapter5.py
+++ think_python/chapter5.py
@@ -5,3 +5,21 @@ hour = int(time // 60)
 minutes = int(time - hour * 60)
 
 print(hour, ":", minutes)
+
+# Como tirar o resto || how to get the remaining
+
+remaining = time - hour * 60
+print(remaining)
+
+
+# Another way
+remaining2 = time % 60
+print(remaining2)
+
+# The module % is also usefull to see if a number is dividable by another if it returns 0 the answer is yes.
+
+number1 = 4
+number2 = 2
+
+dividable = number1 % number2
+print(dividable)
