26aacbe38bd67760c72ac11ce7e9daeedac3a2ce
Ricardo Shinoda <ricardoshinoda@gmail.com>
2024-02-03 10:40:29 -0300

    exer 5.5

--- think_python/exercise5-4.py
+++ think_python/exercise5-4.py
@@ -1,8 +1,12 @@
 def recurse(n, s):
+    """
+    This function will decrease the value assigned to n and using the same ammount, it will increase the same value to s
+    """
     if n == 0:
         print(s)
     else:
         recurse(n-1, n+s)
+        # print(n, s)
 
 
-recurse(-1, 0)
+recurse(3, 0)
