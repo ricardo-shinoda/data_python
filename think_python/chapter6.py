02032ad45dd52c5b75e9e53208836580c98493dd
ricardo-shinoda <ricardoshinoda@gmail.com>
2024-02-07 20:59:10 -0300

    chapter6 WIP

--- think_python/chapter6.py
+++ think_python/chapter6.py
@@ -86,3 +86,37 @@ def factorial(n):
 
 fac_result = factorial(3)
 print(fac_result)
+
+# Fibonacci recursive math function
+
+
+def fibonacci(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+
+
+fibo = fibonacci(6)
+print(fibo)
+
+# Adding another layer of refactor on the Factorial code
+
+
+def factorial2(n):
+    if not isinstance(n, int):
+        print('Factorial is only defined for integers')
+        return None
+    elif n < 0:
+        print('Factorial is not defined for negative integers')
+        return None
+    elif n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+
+fac_result2 = factorial2(-4)
+print(fac_result2)
