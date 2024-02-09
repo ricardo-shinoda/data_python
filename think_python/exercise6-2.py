2899045cb8c8c187540d75587f0800d30b0abf2a
ricardo-shinoda <ricardoshinoda@gmail.com>
2024-02-09 20:26:46 -0300

    palindrome

--- think_python/exercise6-2.py
+++ think_python/exercise6-2.py
@@ -1,11 +1,11 @@
-def ackermann(m, n):
+def ack(m, n):
     if m == 0:
         return n + 1
     elif m > 0 and n == 0:
-        return ackermann(m - 1, 1)
+        return ack(m - 1, 1)
     elif m > 0 and n > 0:
-        return ackermann(m - 1, ackermann(m, n - 1))
+        return ack(m - 1, ack(m, n - 1))
 
 
 # Exemplo de uso
-print(ackermann(5, 4))  # Saída: 125
+print(ack(3, 4))  # Saída: 125
