250241b59f2d141f39d99f1e83bfc014030cf886
ricardo-shinoda <ricardoshinoda@gmail.com>
2024-02-11 18:56:37 -0300

    palindrome wip

--- think_python/palindrome.py
+++ think_python/palindrome.py
@@ -7,31 +7,42 @@ def last(word):
 
 
 def middle(word):
-    return word[2:-2]
+    # return word[2:-2]
+    return len(word) // 2
 
 
 def length(word):
     '''Thinking on work with the lentgh and divide by half and then use it with
     the functions already using on this file'''
     comprimento = len(word)
-    div = int(comprimento / 2)
+    half = comprimento / 2
     print(comprimento)
-    print(div)
+    print(half)
 
 
-letra = length('duas')
+# letra = length('ricardor')
 
-word = 'ricardoshinoda'
+word = 'ricardoshinodar'
 result_middle = middle(word)
-result_fist = first(word)
+result_first = first(word)
 result_last = last(word)
-print('First: ', result_fist)
+print('First: ', result_first)
 print('Middle: ', result_middle)
 print('Last: ', result_last)
 
 
-def is_palidrome(name):
-    print('The name is: ', name)
+def is_palidrome(f, l, m):
+    if f == l:
+        print('This could be a palindrome')
+        print(m)
+        comprimento = m
+        while comprimento >= 2:
+            r = middle(comprimento)
+            comprimento = r
+        print('This is the new middle: ', r)
+    else:
+        print('This is not a palindrome')
+        print(m)
 
 
-is_palidrome('Ricardo')
+is_palidrome(result_first, result_last, result_middle)
