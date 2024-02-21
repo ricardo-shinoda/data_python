0630c89963b7646b0a4c04f19d9a72cab74f316b
ricardo-shinoda <ricardoshinoda@gmail.com>
2024-02-21 20:19:27 -0300

    chapter7

---
+++ think_python/chapter7-squareRoot.py
@@ -0,0 +1,35 @@
+# Square root
+
+a = 4
+x = 3
+y = (x+a/x) / 2
+print(y)
+
+x = y
+y = (x+a/x) / 2
+print(y)
+
+x = y
+y = (x+a/x) / 2
+print(y)
+
+x = y
+y = (x+a/x) / 2
+print(y)
+
+
+x = y
+y = (x+a/x) / 2
+print(y)
+
+
+x = y
+y = (x+a/x) / 2
+print(y)
+
+while True:
+    print(x)
+    y = (x + a/x) / 2
+    if y == x:
+        break
+    x = y
