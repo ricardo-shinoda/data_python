11ef786aa3f840a8d1a1859e4935b516ae3b18d8
ricardo-shinoda <ricardoshinoda@gmail.com>
2024-02-27 21:51:19 -0300

    chapter 8 more lessons

--- think_python/chapter8-strings.py
+++ think_python/chapter8-strings.py
@@ -7,3 +7,31 @@ print(letter)
 
 comprimento = len(fruit)
 print(comprimento)
+
+
+# to get the last letter
+
+last = fruit[comprimento - 1]
+print(last)
+
+# Loop while
+
+index = 0
+while index < len(fruit):
+    letter = fruit[index]
+    print(letter)
+    index = index + 1
+
+# Loop for
+word = 'Pearl Jam'
+for i in word:
+    print(i)
+
+
+# Make way for Ducklings
+
+prefixes = 'JKLMNOPQ'
+suffix = 'ack'
+
+for i in prefixes:
+    print(i + suffix)
