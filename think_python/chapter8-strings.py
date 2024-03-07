2756e50561b84f66ff6ced0d086cd48a4d829cf8
ricardo-shinoda <ricardoshinoda@gmail.com>
2024-03-07 19:43:00 -0300

    reverse

--- think_python/chapter8-strings.py
+++ think_python/chapter8-strings.py
@@ -111,3 +111,22 @@ elif word > 'banana':
     print('Your word, ' + word + ', comes after banana')
 else:
     print('All good, bananas')
+
+
+# Depuração
+
+def is_reverse(word1, word2):
+    if len(word1) != len(word2):
+        return False
+    i = 0
+    j = len(word2)
+    while j > 0:
+        if word1[i] != word2[j]:
+            return False
+        i = i+1
+        j = j-1
+    return True
+
+
+a = is_reverse('pots', 'spot')
+print(a)
