3756d0a0d796d2ea0825066fb1cf07bf2b5cb0e5
ricardo-shinoda <ricardoshinoda@gmail.com>
2024-03-08 21:03:36 -0300

    depuração

--- think_python/chapter8-strings.py
+++ think_python/chapter8-strings.py
@@ -119,8 +119,9 @@ def is_reverse(word1, word2):
     if len(word1) != len(word2):
         return False
     i = 0
-    j = len(word2)
-    while j > 0:
+    j = len(word2)-1
+    while j >= 0:
+        print(i, j)
         if word1[i] != word2[j]:
             return False
         i = i+1
@@ -128,5 +129,5 @@ def is_reverse(word1, word2):
     return True
 
 
-a = is_reverse('pots', 'spot')
+a = is_reverse('pots', 'stop')
 print(a)
